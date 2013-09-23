85f27f2c488490d4cd364be05fdadbfc4d952035
mcron <mcron@hotmail.de>
2013-09-23 21:10:58 +0200

    [SoftwareManager] add image info to full backup

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -1,10 +1,29 @@
-from enigma import *
+#################################################################################
+# FULL BACKUP UYILITY FOR ENIGMA2, SUPPORTS THE MODELS ET-XX00 & VU+			#
+#							& Gigablue & Venton HD Models						#
+#					MAKES A FULLBACK-UP READY FOR FLASHING.						#
+#																				#
+#################################################################################
+from enigma import getBoxType, getMachineBrand, getMachineName, getImageVersionString, getBuildVersionString, getDriverDateString, getEnigmaVersionString
 from Screens.Screen import Screen
 from Components.Button import Button
 from Components.Label import Label
 from Components.ActionMap import ActionMap
+from Components.About import about
 from Screens.Console import Console
+from Screens.MessageBox import MessageBox
+from time import time, strftime, localtime
+from os import path, system, makedirs, listdir, walk, statvfs
+import commands
+import datetime
 
+VERSION = "Version 1.0 openSWF"
+
+def Freespace(dev):
+	statdev = statvfs(dev)
+	space = (statdev.f_bavail * statdev.f_frsize) / 1024
+	print "[FULL BACKUP] Free space on %s = %i kilobytes" %(dev, space)
+	return space
 
 class ImageBackup(Screen):
 	skin = """
@@ -24,13 +43,19 @@ class ImageBackup(Screen):
 	def __init__(self, session, args = 0):
 		Screen.__init__(self, session)
 		self.session = session
+		self.MODEL = getBoxType()
+		self.MACHINENAME = getMachineName()
+		self.MACHINEBRAND = getMachineBrand()
+		print "[FULL BACKUP] BOX MACHINENAME = >%s<" %self.MACHINENAME
+		print "[FULL BACKUP] BOX MACHINEBRAND = >%s<" %self.MACHINEBRAND
+		print "[FULL BACKUP] BOX MODEL = >%s<" %self.MODEL
 		
 		self["key_green"] = Button("USB")
 		self["key_red"] = Button("HDD")
 		self["key_blue"] = Button(_("Exit"))
 		self["key_yellow"] = Button("")
-		self["info-usb"] = Label(_("USB = Do you want to make a back-up on USB?\nThis will take between 2 and 7 minutes depending on the used filesystem and is fully automatic.\nMake sure you first insert an USB flash drive before you select USB."))
-		self["info-hdd"] = Label(_("HDD = Do you want to make an USB-back-up image on HDD? \nThis only takes 2 or 3 minutes and is fully automatic."))
+		self["info-usb"] = Label(_("USB = Do you want to make a back-up on USB?\nThis will take between 4 and 15 minutes depending on the used filesystem and is fully automatic.\nMake sure you first insert an USB flash drive before you select USB."))
+		self["info-hdd"] = Label(_("HDD = Do you want to make an USB-back-up image on HDD? \nThis only takes 2 or 10 minutes and is fully automatic."))
 		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"], 
 		{
 			"blue": self.quit,
@@ -40,17 +65,49 @@ class ImageBackup(Screen):
 			"cancel": self.quit,
 		}, -2)
 
+	def check_hdd(self):
+		if not path.exists("/media/hdd"):
+			self.session.open(MessageBox, _("No /hdd found !!\nPlease make sure you have a HDD mounted.\n"), type = MessageBox.TYPE_ERROR)
+			return False
+		if Freespace('/media/hdd') < 300000:
+			self.session.open(MessageBox, _("Not enough free space on /hdd !!\nYou need at least 300Mb free space.\n"), type = MessageBox.TYPE_ERROR)
+			return False
+		return True
+
+	def check_usb(self, dev):
+		if Freespace(dev) < 300000:
+			self.session.open(MessageBox, _("Not enough free space on %s !!\nYou need at least 300Mb free space.\n" % dev), type = MessageBox.TYPE_ERROR)
+			return False
+		return True
 		
 	def quit(self):
 		self.close()	
 		
 	def red(self):
-		self.session.open(Console, title = "Full back-up on HDD", cmdlist = ["sh '/usr/lib/enigma2/python/Plugins/SystemPlugins/SoftwareManager/backup-hdd.sh'"])
+		if self.check_hdd():
+			self.doFullBackup("/hdd")
 
 	def green(self):
-		self.session.open(Console, title = "Full Back-up to USB", cmdlist = ["sh '/usr/lib/enigma2/python/Plugins/SystemPlugins/SoftwareManager/backup-usb.sh'"])
+		USB_DEVICE = self.SearchUSBcanidate()
+		if USB_DEVICE == 'XX':
+			text = _("No USB-Device found for fullbackup !!\n\n\n")
+			text += _("To back-up directly to the USB-stick, the USB-stick MUST\n")
+			text += _("contain a file with the name: \n\n")
+			text += _("backupstick or backupstick.txt")
+			self.session.open(MessageBox, text, type = MessageBox.TYPE_ERROR)
+		else:
+			if self.check_usb(USB_DEVICE):
+				self.doFullBackup(USB_DEVICE)
 
 	def yellow(self):
 		#// Not used
 		pass	
 
+	def testUBIFS(self):
+		f = open("/proc/mounts", "r")
+		mounts = f.readlines()
+		f.close()
+		for line in mounts:
+			if "rootfs" in line and "ubifs" in line:
+				return "ubifs"
+		return "jffs2"
